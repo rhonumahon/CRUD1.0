d7d4e96951affd2d4025ee636f27222f7d9fcfd3
rhonumahon <65482394+rhonumahon@users.noreply.github.com>
2020-06-19 09:52:49 +0800

    Create main.js

---
+++ public/main.js
@@ -0,0 +1,13 @@
+// main.js
+app.use(express.static('public'));
+const update = document.querySelector('#update-button')
+update.addEventListener('click', _ => {
+    fetch('/quotes', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+        name: 'Darth Vadar',
+        quote: 'I find your lack of faith disturbing.'
+        })
+    })
+    })
